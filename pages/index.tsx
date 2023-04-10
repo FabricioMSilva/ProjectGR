a82a034f3c768b6d5fd001e65c5605870d8a95dc
FABRICIO MARTINS DA SILVA <87395533+FabricioMSilva@users.noreply.github.com>
2023-04-09 21:36:18 -0300

    concerto no todos

--- pages/index.tsx
+++ pages/index.tsx
@@ -13,7 +13,7 @@ export async function getStaticProps() {
   }
 }
 
-export default function Home({todos}) {
+export default function Home({ todos }: { todos: any }) {
 
   return (
     <div className={styles.Home}>
