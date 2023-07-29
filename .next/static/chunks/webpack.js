396740abee86f25f0e322384cd6d5810131ee4cc
FABRICIO MARTINS DA SILVA <87395533+FabricioMSilva@users.noreply.github.com>
2023-07-29 14:28:08 -0300

    teste css

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "60375adfcc6b9048"; }
+/******/ 		__webpack_require__.h = function() { return "3a1b9c8487d162e1"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- .next/static/webpack/webpack.62ae1c316c2f8ba1.hot-update.js
+++ .next/static/webpack/webpack.62ae1c316c2f8ba1.hot-update.js
@@ -11,7 +11,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ function(__webpack_require__) { // webpackRuntimeModules
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "88c967464e7a4145"; }
+/******/ 	__webpack_require__.h = function() { return "3a1b9c8487d162e1"; }
 /******/ }();
 /******/ 
 /******/ }
